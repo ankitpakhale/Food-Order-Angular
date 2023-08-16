1e099a7d6650fd2e0e018f9ca55c5e8f04772b0c
Ankit Pakhale <akp3067@gmail.com>
2023-08-16 21:54:32 +0530

    created: header, home component and food services

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  foods: string[] = [];
+
+  constructor(private fs: FoodService) {
+  }
+
+  ngOnInit() {
+    this.foods = this.fs.getAll(); // getAll function is defined in food service
+  }
+
+}
